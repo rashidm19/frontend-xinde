75e4a4b4933c7d69b64c106962d8f5b2e613c50b
jarvis <maxabdurasulov@gmail.com>
2025-10-20 00:31:30 +0800

    Speaking practice form redesigned

--- src/app/[locale]/practice/speaking/test/page.tsx
+++ src/app/[locale]/practice/speaking/test/page.tsx
@@ -1,16 +1,21 @@
 'use client';
 
 import { GET_practice_speaking_id } from '@/api/GET_practice_speaking_id';
-import { HeaderDuringTest } from '@/components/HeaderDuringTest';
 import { POST_practice_speaking_id_begin } from '@/api/POST_practice_speaking_id_begin';
 import { PracticeLeaveGuard } from '@/components/PracticeLeaveGuard';
 import dynamic from 'next/dynamic';
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useQuery } from '@tanstack/react-query';
+import { WritingFeedbackHeader } from '@/components/practice/WritingFeedbackHeader';
+import { useCustomTranslations } from '@/hooks/useCustomTranslations';
+import { useRouter } from 'next/navigation';
 
 const SpeakingTestForm = dynamic(() => import('./_components/SpeakingTestForm').then(mod => mod.default), { ssr: false });
 
 export default function Page() {
+  const router = useRouter();
+  const { tActions } = useCustomTranslations();
+
   const { data, status } = useQuery({
     queryKey: ['practice-speaking'],
     queryFn: GET_practice_speaking_id,
@@ -22,7 +27,8 @@ export default function Page() {
 
   return (
     <PracticeLeaveGuard>
-      <HeaderDuringTest title='Practice' tag='Speaking' />
+      <WritingFeedbackHeader topBarElevated title={'Practice Speaking'} exitLabel={tActions('exit')} onExit={() => router.push('/profile')} />
+
       {status === 'success' && <SpeakingTestForm data={data} />}
     </PracticeLeaveGuard>
   );
